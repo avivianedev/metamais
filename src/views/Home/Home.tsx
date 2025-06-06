61a25a5373a1fbb10a84ec54b1bdd93995af3efe
avivianedev <developer.viviane@gmail.com>
2025-06-06 20:56:06 -0300

    feat: Pequena alteração em Home

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { useFonts } from "expo-font";
 import { Inter_400Regular, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import Card from '../../components/Card/Card';
+import { clearAllProducts, getData } from '../../controllers/productsController';
+import { useCallback, useEffect, useState } from 'react';
+import { Product } from '../../models/Product';
+import { formatCurrency } from '../../utils/formatCurrency';
+import { useFocusEffect } from '@react-navigation/native';
+
+
+
 
 export default function Home() {
+
+  const [products, setProducts] = useState<Product[]>([]);
+
+  //clearAllProducts()
+
+  useFocusEffect(
+  useCallback(() => {
+
+    const fetchData = async () => {
+      const result = await getData();
+      setProducts(result.sort());
+
+    };
+
+    fetchData()
+
+  }, [])
+
+)
+  console.log('Valor de data', products)
 
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -15,40 +43,32 @@ export default function Home() {
     return null
   }
 
-
-
   return (
     <View style={styles.container}>
-     
-      <Card
-        isLarge={true}
-        title="Crédito Total"
-        percent={74}
-        value={246000}
-        missing={1200.08}
 
+      <FlatList
+
+        data={products}
+        style={styles.list}
+        contentContainerStyle={{
+          paddingBottom: 100,
+          paddingHorizontal: 16,
+          paddingTop: 16,
+        }}
+
+        renderItem={({ item }) => (
+          (
+            <Card
+              isLarge={true}
+              title={item.name}
+              percent={item.percent}
+              value={formatCurrency(item.goal)}
+              missing={formatCurrency(item.remaining)}
+
+            />
+          )
+        )}
       />
-      <Card
-        isLarge={true}
-        title="Consórcio Total"
-        percent={63}
-        value={162000}
-        missing={8064.03} />
-
-      <Card
-        isLarge={true}
-        title="Imobiliário"
-        percent={83}
-        value={162000}
-        missing={8064.03} />
-
-      <Card
-        isLarge={true}
-        title="Imobiliário"
-        percent={83}
-        value={162000}
-        missing={8064.03} />
-
 
       <StatusBar style="auto" />
     </View>
@@ -57,12 +77,17 @@ export default function Home() {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height:'100%',
-    justifyContent:'center',
-    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#F9F9F9', // ou o tom que você quiser   
+    padding: 24,
     gap: 24,
-    
+    width: '100%',
+
+  },
+  list: {
+    marginTop: 20,
+    width: '100%',
+
 
 
   },

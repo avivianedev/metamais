494208ce81d5ad63745a15397c36f1c539e8b174
avivianedev <developer.viviane@gmail.com>
2025-06-13 20:04:41 -0300

    refactor: unifica funções de formatação e sanitização de moeda

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -3,24 +3,21 @@ import { Product } from "../models/Product";
 import { Alert } from "react-native";
 
 
-export const storeData = async (product: Product) : Promise<boolean> => {       
+export const storeData = async (product: Product): Promise<boolean> => {    
 
-    try {        
+    try {
 
         if (!product.name.trim() || !product.segment.trim() || !product.goal) {
             Alert.alert('Campos obrigatórios', 'Preencha todos os campos antes de cadastrar.');
             return false
-        }
-
-        if(product.hasChildren){
-            //
-        }
+        }        
 
         const jsonValue = JSON.stringify(product);
+        console.log('Valor recebido ao clicar em cadastrar Produto,' , jsonValue)
         await AsyncStorage.setItem(`product-${product.id}`, jsonValue);
         console.log('Produto salvo com sucesso!');
-        return true      
-        
+        return true
+
 
     } catch (e) {
         console.log('Erro ao cadastrar Produto', e)
@@ -28,14 +25,14 @@ export const storeData = async (product: Product) : Promise<boolean> => {
     }
 };
 
-export const storeListChildren = ( product : string,  goal: number ) => {
-    const listChildren = []
-    listChildren.push(product)
-    listChildren.push(goal)
-
-    return listChildren
-
-}
+//export const storeListChildren = (product: string, goal: number) => {
+//    const listChildren = []
+//    listChildren.push(product)
+//    listChildren.push(goal)
+//
+//    return listChildren
+//
+//}
 
 export const getData = async () => {
     try {
@@ -51,6 +48,29 @@ export const getData = async () => {
         return [];
     }
 };
+
+export const getItem = async (key: string) => {
+
+    try {
+        const keys = await AsyncStorage.getAllKeys();
+        const productKeys = keys.filter(key => key.startsWith('product-'))
+        const filterKey = productKeys.filter(k => k === `product-${key}`)
+
+
+        if (filterKey) {
+            const stores = await AsyncStorage.multiGet(filterKey);
+            const products = stores.map(([key, value]) => value && JSON.parse(value));            
+            return products
+
+        } else {
+            console.log('O id não foi localizado')
+        }
+
+    } catch (error) {
+        console.log('Erro ao ler o Produto', error)
+        return [];
+    }
+}
 
 
 export const clearAllProducts = async () => {

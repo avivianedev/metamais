ebf85a6c283c933e88dc7ec8a0960da0c590ea94
avivianedev <developer.viviane@gmail.com>
2025-06-20 16:02:03 -0300

    fix: correção na renderização das metas

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,39 +1,44 @@
 import { Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native"
 import { Inter_400Regular } from '@expo-google-fonts/inter';
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { useState } from "react";
-import { formatCurrency } from "../../utils/formatCurrency";
+import { use, useEffect, useState } from "react";
+import { formatCurrency, formatCurrencyInput, sanitizeCurrencyInput } from "../../utils/formatCurrency";
 
 import { useActionSheet } from '@expo/react-native-action-sheet';
 import { EditProductModal } from "../Modal/EditProductModal";
 import { getItem } from "../../controllers/productsController";
 import { Product } from "../../models/Product";
+import { calculateMissing, calculatePercentage } from "../../utils/metricsUtils";
+import { useApp  } from "../context/AppContext";
 
 
 
-const Card = ({ isLarge, id, title, percent, value, missing, children }: any) => {
+const Card = ({ isLarge, id, title, percent, goal, missing, children, produced }: any) => {
 
     const [showChildrenGoals, setShowChildrenGoals] = useState(false);
     const { showActionSheetWithOptions } = useActionSheet();
     const [showEditModal, setShowEditModal] = useState(false)
     const [dataRecoverer, setDataRecoverer] = useState<any | null>(null)
+    const [showNewProductionModal, setShowNewProductionModal] = useState(false)
 
-    //const missingValue = children[0].produced - value
-    //console.log('Children', children)
+    //const {setRefreshList } = useApp();
 
-    const handleGetItem = async (id: string) => {
+    let producedFormater = parseFloat(produced)
+    let valueFormater = parseFloat(goal)
+
+    const handleGetItem = async (id: string, type: 'edit' | 'production') => {
         const selectedProduct = await getItem(id)
-        //console.log('Retorno do selectedProduct que pega o ID', selectedProduct)
         setDataRecoverer(selectedProduct)
+
+        setTimeout(() => {
+            if (type === 'edit') setShowEditModal(true);
+            else setShowNewProductionModal(true);
+        }, 0);
     }
 
-    const calculoPErcentual = (producao: number, metaFinal: number) => {
-        const result = (producao / metaFinal) * 100
-        return result
-    }
 
-    const openOptions = () => {
-        const options = ['Alterar dados', 'Excluir dados', 'Nova meta', 'Cancelar'];
+    const showOptionsMenu = () => {
+        const options = ['Alterar dados', 'Nova Produção', 'Excluir dados', 'Cancelar'];
         const cancelButtonIndex = 3;
 
         showActionSheetWithOptions(
@@ -45,15 +50,14 @@ const Card = ({ isLarge, id, title, percent, value, missing, children }: any) =>
             (selectedIndex) => {
                 switch (selectedIndex) {
                     case 0:
-                        setShowEditModal(true)
-                        handleGetItem(id)
+                        handleGetItem(id, 'edit')
 
                         break;
                     case 1:
-                        // Excluir dados
+                        handleGetItem(id, 'production')
                         break;
                     case 2:
-                        // Inserir nova meta
+                        // nova meta
                         break;
                 }
             }
@@ -70,26 +74,55 @@ const Card = ({ isLarge, id, title, percent, value, missing, children }: any) =>
                 transparent={true}
                 visible={showEditModal}
                 onRequestClose={() => setShowEditModal(false)}
+
             >
                 <EditProductModal
                     onClose={() => setShowEditModal(false)}
                     data={dataRecoverer}
+                    title='Atualizar Produto'
+                    titleHeaderChild="Meta Final"
+                    producedModal={false}
+                    
+
+                />
+            </Modal>
+
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={showNewProductionModal}
+                onRequestClose={() => setShowNewProductionModal(false)}
+
+            >
+                <EditProductModal
+                    onClose={() => setShowNewProductionModal(false)}
+                    data={dataRecoverer}
+                    title='Cadastrar Produção'
+                    titleHeaderChild="Produção"
+                    producedModal={true}
 
                 />
             </Modal>
 
             <TouchableOpacity
-                onLongPress={openOptions}
+                onLongPress={showOptionsMenu}
                 style={[isLarge ? styles.largeCard : styles.smallCard]}
 
 
             >
                 <View style={styles.cardHeader}>
                     <Text style={styles.title}>{title}</Text>
-                    <Text style={styles.title}>% {percent}</Text>
+                    <Text style={styles.title}>% {children?.length 
+                    ? calculatePercentage(children, goal, children) 
+                    : calculatePercentage(producedFormater, valueFormater) }</Text>
                 </View>
-                <Text style={styles.value}>{value}</Text>
-                <Text style={styles.missing}>Falta: {missing}</Text>
+                <Text style={styles.missing}>Falta: {
+                    children?.length
+                        ? formatCurrencyInput(calculateMissing(children, goal, children))
+                        : formatCurrencyInput(calculateMissing(producedFormater, valueFormater))}</Text>
+                <Text style={styles.value}>Meta: {formatCurrencyInput(goal)}
+
+                </Text>
 
                 {children?.length > 0 && (
 
@@ -104,17 +137,18 @@ const Card = ({ isLarge, id, title, percent, value, missing, children }: any) =>
                                 <MaterialIcons name="keyboard-arrow-up" size={24} color="black" />
 
                                 {children.map((child: { name: string, goal: number, produced: number }, index: number) => (
-                                    <View style={styles.childContent}>
+                                    <View style={styles.childContent} key={index}>
 
                                         <View style={styles.childValues} key={index}>
                                             <Text style={styles.childName}>{child.name}</Text>
-                                            {/* <Text style={styles.childGoal}>{formatCurrency(child.goal)}</Text> */}
-                                            <Text style={styles.produced}>{formatCurrency(child.produced)}</Text>
+                                            <Text style={styles.produced}>Prod: {formatCurrency(child.produced)}</Text>
 
 
                                         </View>
                                         <View style={styles.producedValues}>
-                                            <Text style={styles.percent}>{calculoPErcentual(child.produced, child.goal)} %</Text>
+                                            <Text style={styles.childMissing}>Falta: {formatCurrencyInput(calculateMissing(child.produced, child.goal))}</Text>
+                                            <Text style={styles.childPercent}>{calculatePercentage(child.produced, child.goal)} %</Text>
+
                                         </View>
                                     </View>
 
@@ -145,8 +179,9 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 'auto',
         borderRadius: 16,
-        borderColor: '#003459',
-        borderWidth: .8,
+        //borderColor: '#003459',
+        borderColor: '#6c72FF',
+        borderWidth: 1,
         marginBottom: 16
 
 
@@ -170,20 +205,21 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 18,
         fontFamily: 'Inter_400Regular',
-        color: '#3D3D3D',
+        color: '#6c72FF',
+        fontWeight: 700
     },
 
-    value: {
+    missing: {
         fontFamily: 'Inter_400Regular',
-        color: '#3D3D3D',
+        color: '#101935',
         fontSize: 24,
         fontWeight: 700,
 
     },
-    missing: {
+    value: {
         fontSize: 12,
         fontFamily: 'Inter_400Regular',
-        color: '#003459'
+        color: '#212C4D'
     },
     BtnArrow: {
         width: '100%',
@@ -196,14 +232,15 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         gap: 8,
-        
+
+
     },
     childContent: {
-        borderWidth: .8,
-        borderColor: '#fff',
+        borderWidth: 1.5,
+        borderColor: '#6c72FF',
         borderRadius: 10,
         padding: 10,
-        backgroundColor: '#D7C7FF'
+        //backgroundColor: '#D7C7FF'
     },
     childValues: {
         width: '100%',
@@ -214,23 +251,32 @@ const styles = StyleSheet.create({
     },
     childName: {
         fontFamily: 'Inter_400Regular',
-        color: 'black',
+        color: '#6c72FF',
         fontSize: 14,
-        fontWeight: 600,
+        fontWeight: 700,
     },
     produced: {
         fontFamily: 'Inter_400Regular',
-        color: 'black',
+        color: '#101935',
         fontSize: 14,
-        fontWeight: 600,
+        fontWeight: 700,
     },
-    percent: {
+    childPercent: {
         fontFamily: 'Inter_400Regular',
         color: 'black',
         fontSize: 14,
         fontWeight: 600,
     },
+    childMissing: {
+        fontSize: 12,
+        fontWeight: 600,
+        fontFamily: 'Inter_400Regular',
+        color: '#101935'
+    },
     producedValues: {
-        alignSelf: 'flex-end'
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
     }
 })
